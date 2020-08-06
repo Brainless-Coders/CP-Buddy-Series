9a1d09c932e3becf473b9cd5b40e77ad5ccb406c
Vanamala Srikanth <vanamalasrikanth7785@gmail.com>
2020-08-06 20:30:14 +0530

    Another approach for solving 'Unique Number 3'
    Using 'map' Data Structure to solve it.

--- week-1/Editorials/Contest 1/problem-3/solution.cpp
+++ week-1/Editorials/Contest 1/problem-3/solution.cpp
@@ -1,38 +1,23 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<map>
 using namespace std;
-
-
-int main() { 
-    int t; // Input the test cases
-    cin>>t;
-    
-    while(t--) {
-        int n,k; // input n and k (ex 5 and 2)
-        cin>>n>>k;
-        vector<int> v; // Initialize the vector of integers
-        for(int i=0;i<n;i++) {
-            int x;
-            cin>>x;
-            v.push_back(x); // Input the vector elements (ex 6 2 1 1 6)
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        map<int,int> m;
+        int n, k;
+        cin >> n >> k;
+        int a;
+        for(int i = 0; i < n; i++){
+            cin >> a;
+            m[a] = m[a] + 1;
         }
-        
-        sort(v.begin(), v.end()); //  Sort the vector ( 1 1 2 6 6)
-        
-        int count = 1; // Intilize the count as 1
-        
-        for(int i=0;i<n;i++) { // Traverse the vector
-        
-            if(v[i] == v[i+1]) count++; // If number are equal increase the count 
-            else {
-                if(count<k) {  // If occurence of a number is less than k then it is the required output in our case
-                    cout<<v[i]<<endl; // Print that number 
-                    break; // Break from the loop
-                } else {
-                    count = 1; // Else if the occurence is same as k then re-initialize count as 1 and we will continue traversing
-                }
+        for(auto i = m.begin(); i != m.end(); i++){
+            if(i->second == 1){
+                cout << i->first << endl;
+                break;
             }
         }
     }
-    return 0;
 }
-
