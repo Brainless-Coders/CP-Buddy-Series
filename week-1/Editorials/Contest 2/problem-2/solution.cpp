ac4648649a51a9be4514e7815b77df39448edd70
Vanamala Srikanth <vanamalasrikanth7785@gmail.com>
2020-08-10 07:56:02 +0530

    Update solution.cpp
    Added Problem Statement

--- week-1/Editorials/Contest 2/problem-2/solution.cpp
+++ week-1/Editorials/Contest 2/problem-2/solution.cpp
@@ -1,3 +1,31 @@
+/*
+Given a string of character, find the length of longest proper prefix which is also a proper suffix. Example: S = abab lps is 2 because, ab.. is prefix and ..ab is also a suffix.
+
+Input Format
+
+First line is T number of test cases. 1<=T<=100. Each test case has one line denoting the string of length less than 100000.
+
+Constraints
+
+1<=T<=100
+Output Format
+
+Print length of longest proper prefix which is also a proper suffix.
+
+Sample Input 0
+
+2
+abab
+aaaa
+Sample Output 0
+
+2
+3
+Explanation 0
+
+S = abab lps is 2 because, ab.. is prefix and ..ab is also a suffix.
+*/
+
 #include<iostream>
 #include<string.h>
 using namespace std;
